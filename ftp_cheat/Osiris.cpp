10b06a4c525efd7ed954b83ecff90044fc508a89
n0verify <icyngu@gmail.com>
2021-07-03 10:35:15 -0700

    upgrade detection

--- ftp_cheat/Osiris.cpp
+++ ftp_cheat/Osiris.cpp
@@ -6,6 +6,8 @@
 #endif
 
 #include "Hooks.h"
+#include "def.h"
+#include "xorstr.hpp"
 
 #ifdef _WIN32
 
@@ -13,11 +15,15 @@ extern "C" BOOL WINAPI _CRT_INIT(HMODULE moduleHandle, DWORD reason, LPVOID rese
 
 BOOL APIENTRY DllEntryPoint(HMODULE moduleHandle, DWORD reason, LPVOID reserved)
 {
+    VMP_ULTRA(xorstr_("DllMain"));
+
     if (!_CRT_INIT(moduleHandle, reason, reserved))
         return FALSE;
 
     if (reason == DLL_PROCESS_ATTACH) {
-        std::setlocale(LC_CTYPE, ".utf8");
+        //std::setlocale(LC_CTYPE, ".utf8");
+        VMP_ULTRA(xorstr_("OnDllAttach"));
+        std::setlocale(LC_CTYPE, xorstr_(".utf8"));
         hooks = std::make_unique<Hooks>(moduleHandle);
     }
     return TRUE;
@@ -27,7 +33,9 @@ BOOL APIENTRY DllEntryPoint(HMODULE moduleHandle, DWORD reason, LPVOID reserved)
 
 void __attribute__((constructor)) DllEntryPoint()
 {
+    VMP_ULTRA(xorstr_("DllMain"));
     hooks = std::make_unique<Hooks>();
+    VMP_END;
 }
 
 #endif
