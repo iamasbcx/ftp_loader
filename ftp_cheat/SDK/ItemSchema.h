12c0c9f315cd604d8d897d9f03b9c5dc29392640
n0verify <icyngu@gmail.com>
2021-07-21 06:22:52 -0700

    move implementations

--- ftp_cheat/SDK/ItemSchema.h
+++ ftp_cheat/SDK/ItemSchema.h
@@ -8,58 +8,12 @@
 #include "UtlVector.h"
 #include "VirtualMethod.h"
 #include "UtlMemory.h"
+#include "UtlMap.h"
+#include "UtlString.h"
 
 #include "../Memory.h"
 
 enum class WeaponId : short;
-
-template <typename Key, typename Value>
-struct Node {
-    int left;
-    int right;
-    int parent;
-    int type;
-    Key key;
-    Value value;
-};
-
-template <typename Key, typename Value>
-struct UtlMap {
-    auto begin() const noexcept { return memory.memory; }
-    auto end() const noexcept { return memory.memory + numElements; }
-    
-    int find(Key key) const noexcept
-    {
-        auto curr = root;
-
-        while (curr != -1) {
-            const auto el = memory[curr];
-
-            if (el.key < key)
-                curr = el.right;
-            else if (el.key > key)
-                curr = el.left;
-            else
-                break;
-        }
-        return curr;
-    }
-
-    void* lessFunc;
-    UtlMemory<Node<Key, Value>> memory;
-    int root;
-    int numElements;
-    int firstFree;
-    int lastAlloc;
-    Node<Key, Value>* elements;
-};
-
-struct UtlString {
-    UtlMemory<char> buffer;
-    int length;
-
-    const char* data() const noexcept { return buffer.memory; }
-};
 
 struct PaintKit {
     int id;
